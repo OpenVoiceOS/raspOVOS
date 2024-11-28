b35024a00ecf6bc78cab93def9e058eaa1281da6
miro <jarbasai@mailfence.com>
2024-11-28 03:51:20 +0000

    define .conf files to merge via ENV vars

--- build_raspOVOS_ca.sh
+++ build_raspOVOS_ca.sh
@@ -40,7 +40,8 @@ jq_command="jq -s"
 # Loop through the list of files from CONFIG_FILES
 IFS=',' read -r -a config_files <<< "$CONFIG_FILES"
 for file in "${config_files[@]}"; do
-  jq_command="$jq_command /mounted-github-repo/$file"
+  # Properly quote file paths to prevent shell issues
+  jq_command="$jq_command \"/mounted-github-repo/$file\""
 done
 # Merge all the files using jq and save to /etc/mycroft/mycroft.conf
 eval "$jq_command > /etc/mycroft/mycroft.conf"
