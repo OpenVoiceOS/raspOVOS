0226c0d861289c36ee598d48cc02bcb39f2ec7df
miro <jarbasai@mailfence.com>
2024-12-03 15:59:31 +0000

    add galician image

---
+++ build_raspOVOS_gl.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Exit on error
+# If something goes wrong just stop.
+# it allows the user to see issues at once rather than having
+# scroll back and figure out what went wrong.
+set -e
+
+# Activate the virtual environment
+source /home/$USER/.venvs/ovos/bin/activate
+
+echo "Downloading portuguese vosk model..."
+# Download and extract VOSK model
+VOSK_DIR="/home/$USER/.local/share/vosk"
+mkdir -p $VOSK_DIR
+wget https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip -P $VOSK_DIR
+unzip -o $VOSK_DIR/vosk-model-small-pt-0.3.zip -d $VOSK_DIR
+rm $VOSK_DIR/vosk-model-small-pt-0.3.zip
+
+# remove english piper voice
+EN_PIPER_DIR="/home/$USER/.local/share/piper_tts/voice-en-gb-alan-low"
+rm -rf "$EN_PIPER_DIR"
+
+# TODO cotovia
+
+echo "Creating system level mycroft.conf..."
+mkdir -p /etc/mycroft
+
+CONFIG_ARGS=""
+# Loop through the MYCROFT_CONFIG_FILES variable and append each file to the jq command
+IFS=',' read -r -a config_files <<< "$MYCROFT_CONFIG_FILES"
+for file in "${config_files[@]}"; do
+  CONFIG_ARGS="$CONFIG_ARGS /mounted-github-repo/$file"
+done
+# Execute the jq command and merge the files into mycroft.conf
+jq -s 'reduce .[] as $item ({}; . * $item)' $CONFIG_ARGS > /etc/mycroft/mycroft.conf
+
+
+echo "Ensuring permissions for $USER user..."
+# Replace 1000:1000 with the correct UID:GID if needed
+chown -R 1000:1000 /home/$USER
+
+echo "Cleaning up apt packages..."
+apt-get --purge autoremove -y && apt-get clean
