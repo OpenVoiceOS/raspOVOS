258e49253d149fd1ffeb5b72fd7f6cc4774bbbec
miro <jarbasai@mailfence.com>
2024-12-14 14:50:18 +0000

    pt citrinet fallback + dutch image

--- build_raspOVOS_nl.sh
+++ build_raspOVOS_nl.sh
@@ -10,39 +10,42 @@ source /home/$USER/.venvs/ovos/bin/activate
 
 
 echo "Setting up default wifi country..."
-/usr/bin/raspi-config nonint do_wifi_country PT
+/usr/bin/raspi-config nonint do_wifi_country NL
 
 echo "Caching pre-trained padatious intents..."
 mkdir -p /home/$USER/.local/share/mycroft/intent_cache
-cp -rv /mounted-github-repo/intent_cache/pt-PT /home/$USER/.local/share/mycroft/intent_cache/
+cp -rv /mounted-github-repo/intent_cache/nl-NL /home/$USER/.local/share/mycroft/intent_cache/
 
-echo "Downloading portuguese citrinet model..."
-python /mounted-github-repo/packages/download_citrinet_pt.py
+echo "Downloading dutch citrinet model..."
+python /mounted-github-repo/packages/download_citrinet_nl.py
 # since script was run as root, we need to move downloaded files
 mkdir -p /home/ovos/.cache/huggingface/hub/
-mv /root/.cache/huggingface/hub/models--neongeckocom--stt_pt_citrinet_512_gamma_0_25/ /home/ovos/.cache/huggingface/hub/models--neongeckocom--stt_pt_citrinet_512_gamma_0_25/
+mv /root/.cache/huggingface/hub/models--neongeckocom--stt_nl_citrinet_512_gamma_0_25/ /home/ovos/.cache/huggingface/hub/models--neongeckocom--stt_nl_citrinet_512_gamma_0_25/
+
+echo "Downloading dutch vosk model..."
+# Download and extract VOSK model
+VOSK_DIR="/home/$USER/.local/share/vosk"
+mkdir -p $VOSK_DIR
+wget https://alphacephei.com/vosk/models/vosk-model-small-nl-0.22.zip -P $VOSK_DIR
+unzip -o $VOSK_DIR/vosk-model-small-nl-0.22.zip -d $VOSK_DIR
+rm $VOSK_DIR/vosk-model-small-nl-0.22.zip
 
 
 echo "Installing Piper TTS..."
 uv pip install --no-progress ovos-tts-plugin-piper -c $CONSTRAINTS
 
-echo "Downloading portuguese vosk model..."
-# Download and extract VOSK model
-VOSK_DIR="/home/$USER/.local/share/vosk"
-mkdir -p $VOSK_DIR
-wget https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip -P $VOSK_DIR
-unzip -o $VOSK_DIR/vosk-model-small-pt-0.3.zip -d $VOSK_DIR
-rm $VOSK_DIR/vosk-model-small-pt-0.3.zip
-
-# download default piper voice for portuguese
-PIPER_DIR="/home/$USER/.local/share/piper_tts/tugao-medium"
-VOICE_URL="https://huggingface.co/rhasspy/piper-voices/resolve/main/pt/pt_PT/tug%C3%A3o/medium/pt_PT-tug%C3%A3o-medium.onnx"
-VOICE_URL2="https://huggingface.co/rhasspy/piper-voices/resolve/main/pt/pt_PT/tug%C3%A3o/medium/pt_PT-tug%C3%A3o-medium.onnx.json"
+# download default piper voice for dutch
+PIPER_DIR="/home/$USER/.local/share/piper_tts/mls_5809-low"
+VOICE_URL="https://github.com/rhasspy/piper/releases/download/v0.0.2/voice-nl-mls_5809-low.tar.gz"
+VOICE_ARCHIVE="$PIPER_DIR/voice-nl-mls_5809-low.tar.gz"
 mkdir -p "$PIPER_DIR"
 echo "Downloading voice from $VOICE_URL..."
-wget "$VOICE_URL" -O "$PIPER_DIR/pt_PT-tugão-medium.onnx"
-echo "Downloading voice config from $VOICE_URL2..."
-wget "$VOICE_URL2" -O "$PIPER_DIR/pt_PT-tugão-medium.onnx.json"
+wget "$VOICE_URL" -O "$VOICE_ARCHIVE"
+tar -xvzf "$VOICE_ARCHIVE" -C "$PIPER_DIR"
+# if we remove the voice archive the plugin will think its missing and redownload voice on boot...
+rm "$VOICE_ARCHIVE"
+touch $VOICE_ARCHIVE
+
 
 echo "Creating system level mycroft.conf..."
 mkdir -p /etc/mycroft
