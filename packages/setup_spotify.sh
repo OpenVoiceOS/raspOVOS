a94feee894b33e6aaf0fc061b56283a54f9b6f4a
miro <jarbasai@mailfence.com>
2024-12-27 00:55:08 +0000

    still debugging rust stuff...

--- packages/setup_spotify.sh
+++ packages/setup_spotify.sh
@@ -8,13 +8,22 @@ echo "Installing Rust..."
 # Set RUSTUP_HOME and CARGO_HOME
 export RUSTUP_HOME="/usr/bin"
 export CARGO_HOME="/home/$USER/.cargo"
-mkdir -p $CARGO_HOME
+mkdir -p "$CARGO_HOME"
 
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable --profile minimal --no-modify-path
-. "$CARGO_HOME/env"
+
+# Source the Cargo environment to update PATH
+source "$CARGO_HOME/env"
+
+# Verify that Cargo is in the PATH
+if ! command -v cargo &> /dev/null; then
+    echo "Cargo could not be found. Please check the installation."
+    exit 1
+fi
 
 echo "Installing librespot..."
-usr/bin/cargo install librespot
+cargo install librespot
 
 # Install OVOS spotifyd hook script
 echo "Installing spotifyd OCP hooks..."
