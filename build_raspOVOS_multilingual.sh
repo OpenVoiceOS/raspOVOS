4f1ad6a78ab572d073e16f4af8816d0cec0914d1
miro <jarbasai@mailfence.com>
2025-01-29 02:40:11 +0000

    multilingual image

--- build_raspOVOS_multilingual.sh
+++ build_raspOVOS_multilingual.sh
@@ -9,17 +9,11 @@ set -e
 source /home/$USER/.venvs/ovos/bin/activate
 
 echo "Copying overlays..."
+sudo cp -rv /mounted-github-repo/overlays/en/* /
 sudo cp -rv /mounted-github-repo/overlays/multi/* /
 
-echo "Downloading whisper model..."
-python -c "from huggingface_hub import snapshot_download; repo_id = 'Systran/faster-whisper-tiny'; file_path = snapshot_download(repo_id=repo_id); print(f'Downloaded {repo_id}'); print(file_path)"
-# since script was run as root, we need to move downloaded files
-mkdir -p /home/ovos/.cache/huggingface/hub/
-mv /root/.cache/huggingface/hub/models--Systran--faster-whisper-tiny/ /home/ovos/.cache/huggingface/hub/models--Systran--faster-whisper-tiny/
-
-echo "Installing Piper TTS..."
-uv pip install --no-progress ovos-tts-plugin-piper -c $CONSTRAINTS
-
+echo "Installing Edge TTS..." # TODO: only multilingual model currently
+uv pip install --no-progress ovos-tts-plugin-edge-tts -c $CONSTRAINTS
 
 echo "Ensuring permissions for $USER user..."
 # Replace 1000:1000 with the correct UID:GID if needed
