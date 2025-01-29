64c21405ce66c87384a4ef370f458cc1a6d96bef
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-01-29 16:50:23 +0000

    multilingual image (#134)
    * local whisper
    
    * multilingual image
    
    * multilingual image
    
    * multilingual image

---
+++ build_raspOVOS_multilingual.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Exit on error
+# If something goes wrong just stop.
+# it allows the user to see issues at once rather than having
+# scroll back and figure out what went wrong.
+set -e
+
+# Activate the virtual environment
+source /home/$USER/.venvs/ovos/bin/activate
+
+echo "Copying overlays..."
+sudo cp -rv /mounted-github-repo/overlays/en/* /
+sudo cp -rv /mounted-github-repo/overlays/multi/* /
+
+echo "Installing Edge TTS..." # TODO: only multilingual model currently
+uv pip install --no-progress ovos-tts-plugin-edge-tts -c $CONSTRAINTS
+
+echo "Ensuring permissions for $USER user..."
+# Replace 1000:1000 with the correct UID:GID if needed
+chown -R 1000:1000 /home/$USER
+
+echo "Cleaning up apt packages..."
+apt-get --purge autoremove -y && apt-get clean
