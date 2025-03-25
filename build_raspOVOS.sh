f0ea925f52f40eb1fd4168f96f33ecf123ef10ca
miro <jarbasai@mailfence.com>
2025-03-25 19:50:31 +0000

    move espeak to base image

--- build_raspOVOS.sh
+++ build_raspOVOS.sh
@@ -77,6 +77,13 @@ pip install sdnotify uv --break-system-packages
 echo "Installing admin phal..."
 pip install ovos-bus-client ovos-phal ovos-PHAL-plugin-system -c $CONSTRAINTS --break-system-packages
 
+echo "Compiling latest espeak..."
+apt-get install -y jq automake libtool
+git clone https://github.com/espeak-ng/espeak-ng.git /tmp/espeak-ng
+cd /tmp/espeak-ng
+./autogen.sh  && ./configure && make && make install
+rm -rf /tmp/espeak-ng
+
 # Create and activate a virtual environment for OVOS.
 echo "Creating virtual environment..."
 mkdir -p /home/$USER/.venvs
