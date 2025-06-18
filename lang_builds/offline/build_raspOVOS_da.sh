ea55cbcf0b4e41502c600cd2fad63aa9e04febd9
miro <jarbasai@mailfence.com>
2025-06-18 06:58:35 +0100

    add danish image

---
+++ lang_builds/offline/build_raspOVOS_da.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Exit on error
+# If something goes wrong just stop.
+# it allows the user to see issues at once rather than having
+# scroll back and figure out what went wrong.
+set -e
+
+## Intended to run on top of the raspOVOS FULL image
+: "${OVOS_USER:=ovos}"
+: "${CONSTRAINTS:=https://github.com/OpenVoiceOS/ovos-releases/raw/refs/heads/main/constraints-alpha.txt}"
+
+# start from hybrid image
+bash /mounted-github-repo/lang_builds/hybrid/build_raspOVOS_da.sh
+
+# Activate the virtual environment
+source /home/$OVOS_USER/.venvs/ovos/bin/activate
+
+echo "Configuring for target language..."
+/home/$OVOS_USER/.venvs/ovos/bin/ovos-config autoconfigure --lang de-DE --offline --male --platform rpi5
+
+echo "Downloading base whisper model ..."
+python -c "from huggingface_hub import snapshot_download; repo_id = 'Systran/faster-whisper-base'; file_path = snapshot_download(repo_id=repo_id); print(f'Downloaded {repo_id}')"
+# since script was run as root, we need to move downloaded files
+mv /root/.cache/huggingface/hub/models--Systran--faster-whisper-base/ /home/ovos/.cache/huggingface/hub/models--Systran--faster-whisper-base/
+
+echo "Ensuring permissions for $OVOS_USER user..."
+# Replace 1000:1000 with the correct UID:GID if needed
+chown -R 1000:1000 /home/$OVOS_USER
